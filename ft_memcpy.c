b6492c8ffb7644bcd74ed819f5bbb0036c0eb92f
Diogo Dias <diogodiasgrilo@gmail.com>
2023-03-06 16:27:45 +0200

    Standard functions needed for libft.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,25 @@
 /*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 14:42:42 by diogpere          #+#    #+#             */
-/*   Updated: 2023/03/05 14:42:43 by diogpere         ###   ########.fr       */
+/*   Updated: 2023/03/06 08:25:16 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(char *dst, const char *src, int n)
+void	*ft_memcpy(void *dst, const void *src, int n)
 {
-	int	i;
+	int		i;
+	char	*c_dst;
+	char	*c_src;
 
 	i = 0;
-	if (dst == src)
+	c_dst = (char *)dst;
+	c_src = (char *)src;
+	if (c_dst == c_src)
 		return (dst);
 	while (i < n)
 	{
-		dst[i] = src[i];
+		c_dst[i] = c_src[i];
 		i++;
 	}
-	return (dst);
+	return ((void *)c_dst);
 }
