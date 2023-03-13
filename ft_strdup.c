c7048e13d098d1519ea8a4b2ad55983bacf9663c
Diogo Dias <diogodiasgrilo@gmail.com>
2023-03-13 11:19:43 -0600

    Standard functions needed for libft.

--- ft_strdup.c
+++ ft_strdup.c
@@ -12,23 +12,13 @@
 
 #include "libft.h"
 
-static int	ft_str_length(char *str)
-{
-	int	index;
-
-	index = 0;
-	while (str[index])
-		index++;
-	return (index);
-}
-
 char	*ft_strdup(char *src)
 {
 	int		index;
 	char	*dest;
 
 	index = 0;
-	dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1);
+	dest = (char *)malloc(ft_strlen(src) * sizeof(char) + 1);
 	if (!dest)
 		return (0);
 	while (src[index])
